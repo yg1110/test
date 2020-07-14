1013f1c21984d41a3977144c7328bee30b0480b2
yg1110 <younggil9488@gmail.com>
2020-07-14 23:50:47 +0900

    메인화면 제작중

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="top">
+        <h1>
+          <a href="#" title="Katosnap">
+            YG
+          </a>
+        </h1>
       </header>
+
+      <nav className="flex-nav">
+        <a href="#" className="toggleNav">
+          <i className="fa fa-bars"></i> Menu
+        </a>
+        <ul>
+          <li>
+            <a href="#" title="Home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" title="About">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" title="Work">
+              Project
+            </a>
+          </li>
+          <li className="social">
+            <a href="https://github.com/kathykato" title="GitHub">
+              <i className="fa fa-github"></i>
+            </a>
+          </li>
+          <li className="social">
+            <a href="https://github.com/kathykato" title="GitHub">
+              <i className="fa fa-tumblr"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <section className="gallery">
+        <ul>
+          <li className="gallery-image">
+            <img src="https://source.unsplash.com/hclTKUfuf1U" alt="Katosnap" />
+          </li>
+          <li className="gallery-image">
+            <img src="https://source.unsplash.com/DOExRUPPwYY" alt="Katosnap" />
+          </li>
+          <li className="gallery-image">
+            <img src="https://source.unsplash.com/a6x7YPiNxwI" alt="Katosnap" />
+          </li>
+          <li className="gallery-image">
+            <img src="https://source.unsplash.com/AiORnUT6sC0" alt="Katosnap" />
+          </li>
+          <li className="gallery-image">
+            <img src="https://source.unsplash.com/DGVA6sqYdUA" alt="Katosnap" />
+          </li>
+        </ul>
+      </section>
+
+      <section className="details">
+        <h2>Sign up today for the latest updates!</h2>
+      </section>
+
+      <footer>
+        <p>&copy; yg1110</p>
+      </footer>
     </div>
   );
 }
