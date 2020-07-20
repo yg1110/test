4d0af0c18c588bbd63ff32db7fc3bdf46e45c84e
yg1110 <younggil9488@gmail.com>
2020-07-20 22:45:02 +0900

    webpack 이미지 추가

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./style.css";
+import img1 from "./images/냉장고를부탁해.png";
+import img2 from "./images/ItForYou.png";
+import img3 from "./images/Newspace.png";
+import img4 from "./images/Ssafy-Shopper.png";
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,12 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "file-loader"
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+              context: "src/",
+              publicPath: "images/"
+            }
           }
         ]
       }
