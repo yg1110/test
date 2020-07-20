04a633c4e2949d3da353320225c349e5bab5392f
yg1110 <younggil9488@gmail.com>
2020-07-21 01:53:20 +0900

    modal 1차완료

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./style.css";
-import img1 from "./images/냉장고를부탁해.png";
-import img2 from "./images/ItForYou.png";
-import img3 from "./images/Newspace.png";
-import img4 from "./images/Ssafy-Shopper.png";
+import "./modal.css";
+import "./button.scss";
+
+import "./images/냉장고를부탁해.png";
+import "./images/ItForYou.png";
+import "./images/Newspace.png";
+import "./images/Ssafy-Shopper.png";
