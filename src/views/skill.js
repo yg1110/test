0d89ce72c89769c9c8027854956b4d25c00f32df
yg1110 <younggil9488@gmail.com>
2020-07-26 23:30:44 +0900

    skil 추가

--- src/views/skill.js
+++ src/views/skill.js
@@ -70,10 +70,103 @@ class skill extends HTMLElement {
       <div class="skill-description">
         <div class="skill-description-name">기초문법</div>
         <div class="skill-description-link">
-            <div>배열함수 선언 및 사용 가능</div>
+            <div>배열함수(map, reduce 등) 선언 및 사용 가능</div>
             <div>객체 선언 및 사용 가능</div>
+            <div>Class 선언 및 사용 가능</div>
             <div>this와 스코프에 대한 이해</div>
+            <div>클로저에 대한 이해</div>
+            <div>모듈화 가능</div>
         </div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">DOM 조작</div>
+        <div class="skill-description-link">
+            <div>태그 생성 및 수정 가능</div>
+            <div>DOM에 접근하여 스타일 변경가능</div>
+            <div>버튼 이벤트 선언 가능</div>
+        </div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">Vanilla 프로젝트 경험</div>
+        <div class="skill-description-link">
+          <div>개인 프로젝트 1회(현재 페이지)</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="skill-content">
+      <h2 class="skill-title">Webpack & Babel</h2>
+      <div class="skill-description">
+        <div class="skill-description-name">webpack</div>
+        <div class="skill-description-link">
+            <div>모듈 번들링하여 개발환경 구성 가능</div>
+        </div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">Babel</div>
+        <div class="skill-description-link">
+          <div>ES6와 ES5문법에 대한 이해 및 개발환경구성 가능</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="skill-content">
+      <h2 class="skill-title">Vue.js</h2>
+      <div class="skill-description">
+        <div class="skill-description-name">라이프 사이클</div>
+        <div class="skill-description-link">라이프 사이클 이해 및 활용가능(비동기적 특성 해결 등)</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">데이터 전달</div>
+        <div class="skill-description-link">Props와 Emit을 이용하여 부모, 자식간 데이터 전달 가능</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">상태관리</div>
+        <div class="skill-description-link">Vuex을 이용한 상태관리 가능</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">서버통신</div>
+        <div class="skill-description-link">axios, XMLHttpRequest를 이용하여 서버와 통신 가능</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">프로젝트 경험</div>
+        <div class="skill-description-link">삼성 청년소프트웨어 아카데미(SSAFY) 협업 프로젝트 4회 (수상 2회)</div>
+      </div>
+    </div>
+
+    <div class="skill-content">
+      <h2 class="skill-title">React</h2>
+      <div class="skill-description">
+        <div class="skill-description-name">state 조작</div>
+        <div class="skill-description-link">Class와 Hook버전 모두 State 조작 가능</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">라이프 사이클</div>
+        <div class="skill-description-link">Class 라이프사이클, Hooks useEffect 사용 가능</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">최적화</div>
+        <div class="skill-description-link">memo, useMemo, useCallback 사용 가능</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">데이터 전달</div>
+        <div class="skill-description-link">Reducer, ContextAPI를 이용한 데이터 전달 가능</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">프로젝트 경험</div>
+        <div class="skill-description-link">개인 프로젝트 1회(Todo List)</div>
+      </div>
+    </div>
+
+    <div class="skill-content">
+      <h2 class="skill-title">협업 툴</h2>
+      <div class="skill-description">
+        <div class="skill-description-name">GIT</div>
+        <div class="skill-description-link">협업시 브렌치를 따로 나누어 개발한 경험 있음</div>
+      </div>
+      <div class="skill-description">
+        <div class="skill-description-name">JIRA & Notion</div>
+        <div class="skill-description-link">협업 프로젝트 계획을 세울때 사용한 경험 있습니다.</div>
       </div>
     </div>
 
